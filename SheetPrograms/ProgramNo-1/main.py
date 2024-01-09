d09a9495a231c41b8c46836712d795b8b24ee230
Faysal_Ezaz <95119493+Faysal-Ezaz@users.noreply.github.com>
2024-01-10 01:12:18 +0530

    Update main.py

--- SheetPrograms/ProgramNo-1/main.py
+++ SheetPrograms/ProgramNo-1/main.py
@@ -1,3 +1,4 @@
+# Read a text file line by line and display each word separated by a '#'
 with open('your_text_file.txt', 'r') as file:  # Open the file in read mode
   for line in file:  # Iterate over each line in the file
     words = line.split()  # Split the line into words
